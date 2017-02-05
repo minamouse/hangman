dc4ad52e95e3a04d2b70310282bf57c9cd445742
Marina <marinacottrell@gmail.com>
2017-02-05 14:08:21 -0800

    finished basic game with ascii

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, session
+from flask import Flask, render_template, jsonify, request, session, redirect, flash
 import os
 import random
 
@@ -14,6 +14,9 @@ def index():
     if 'word' not in session:
         session['word'] = random.choice(WORDS)
         session['guesses'] = []
+        session['bad_guesses'] = []
+        session['qty_guesses'] = 6
+        session['finished'] = False
 
     return render_template('index.html')
 
@@ -26,20 +29,52 @@ def check_guess():
             letters.append(l)
         else:
             letters.append('_')
-
     return letters
 
 
 @app.route('/guess.json', methods=['POST'])
 def guess():
 
-    guess = request.form.get('guess')
-    session['guesses'] = session['guesses'] + [guess]
+    guess = request.form.get('guess', None)
+    message = None
+    if guess:
+        guess = guess.lower()
+        session['guesses'] = session['guesses'] + [guess]
+        if guess not in session['word']:
+            if guess in session['bad_guesses']:
+                message = 'You already guessed that letter!'
+            else:
+                session['bad_guesses'] = session['bad_guesses'] + [guess]
 
     letters = check_guess()
+    victory = ''.join(letters) == session['word']
+    if session['qty_guesses'] == len(session['bad_guesses']) or victory:
+        session['finished'] = True
 
     return jsonify({'letters': letters,
-                    'all_guesses': session['guesses']})
+                    'bad_guesses': session['bad_guesses'],
+                    'victory': victory,
+                    'finished': session['finished'],
+                    'message': message})
+
+
+@app.route('/reset', methods=['POST'])
+def reset():
+
+    session.pop('word')
+    session.pop('guesses')
+    session.pop('qty_guesses')
+
+    return redirect('/')
+
+
+@app.route('/get_word')
+def get_word():
+
+    if session['finished']:
+        return jsonify({'word': session['word']})
+    else:
+        return 'none of your business'
 
 if __name__ == '__main__':
 
--- static/game.js
+++ static/game.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var guessBtn = $('#guess_input');
+$.post('/guess.json', {}, updateGame);
 
 guessBtn.on('submit', function (evt) {
     evt.preventDefault();
@@ -11,15 +12,67 @@ guessBtn.on('submit', function (evt) {
     $.post('/guess.json', data, updateGame);
 });
 
+function updateVictory(victory) {
+    if (victory) {
+        $('#victory_status').html('You Win!!!');
+    } else {
+        $('#victory_status').html('You Lose!!!');
+        showWord();
+    }
+    $('#guess_input_text').attr('disabled', true);
+    $('#submit_button').attr('disabled', true);
+}
+
+function resetGame() {
+
+    $('#victory_status').html('');
+    $('#guess_input_text').attr('disabled', false);
+    $('#submit_button').attr('disabled', false);
+
+}
+
+function showWord() {
+    $.get('/get_word', function(response){
+        var word = response.word.split('');
+        var guesses = $('#current_word').html().split(' ');
+        console.log(guesses);
+        var coloredLetters = [];
+        for (var i = 0; i < word.length; i++) {
+            if (word[i] === guesses[i]) {
+                coloredLetters.push(word[i]);
+            } else {
+                coloredLetters.push('<span class="incorrect">' + word[i] + '</span>');
+            }
+        }
+        $('#current_word').html(coloredLetters.join(' '));
+    });
+}
+
 function updateGame(response) {
     $('#guess_input_text').val('');
 
-    updateGuessedLetters(response.all_guesses);
+    updateGuessedLetters(response.bad_guesses);
     updateWord(response.letters);
+    if (response.finished) {
+        updateVictory(response.victory);
+    }
+    updateAscii(response.bad_guesses.length);
+    if (response.message) {
+        $('#message').html(response.message);
+    } else {
+        $('#message').html('');
+    }
 }
 
 function updateGuessedLetters(guesses) {
     $('#all_guesses').html(guesses.join(' '));
+}
+
+function updateAscii(qty) {
+    var id_num = '#n' + qty;
+    console.log(id_num);
+    $('pre').attr('hidden', true);
+    $(id_num).attr('hidden', false);
 }
 
 function updateWord(letters) {
